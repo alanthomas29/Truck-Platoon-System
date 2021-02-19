cb1be975c5ce8e5e251f7ac82f8c788655b18075
ninad9 <78981903+ninad9@users.noreply.github.com>
2021-02-20 04:39:16 +0530

    initial commit
    base structure

---
+++ TruckPlatoonSystem/src/com/utiltity/DataInputDto.java
@@ -0,0 +1,67 @@
+package com.utiltity;
+
+import java.io.Serializable;
+
+public class DataInputDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+
+	private int speed;
+	private int vGap;
+	private String operation;
+	private int steerAngle;
+	private int destDistance;
+	
+	
+
+	public int getDestDistance() {
+		return destDistance;
+	}
+
+	public void setDestDistance(int destDistance) {
+		this.destDistance = destDistance;
+	}
+
+	public int getSteerAngle() {
+		return steerAngle;
+	}
+
+	public void setSteerAngle(int steerAngle) {
+		this.steerAngle = steerAngle;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getvGap() {
+		return vGap;
+	}
+
+	public void setvGap(int vGap) {
+		this.vGap = vGap;
+	}
+
+	@Override
+	public String toString() {
+		return "DataInputDto: {speed=" + getSpeed() + "vGap=" + getvGap() + "operation=" + getOperation() + "}";
+
+	}
+
+}
---
+++ TruckPlatoonSystem/src/com/utiltity/StringConstants.java
@@ -0,0 +1,14 @@
+package com.utiltity;
+
+public class StringConstants {
+
+	public static final String BRAKE = "brake";
+	public static final String INITIATE = "initiate";
+	public static final String RESTART = "restart";
+	public static final String CLIENTBRAKE = "clBrake";
+	public static final String CLIENTACK = "ack";
+	public static final String CLIENT_OBSTACLE_DETECTED = "obstacle detected";
+	public static final String SMALL_DETECTED = "LMV";
+	public static final String LARGE_DETECTED = "HMV";
+	public static final String NO_OBSTACLE = "no obstacle";
+}
